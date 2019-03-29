a1a965e2054690e92a57baacba728fcb02b2f90c
Tamara <97730@student.drenthecollege.nl>
2019-03-29 09:31:10 +0100

    Added 30 words

--- ui/src/views/homePage.vue
+++ ui/src/views/homePage.vue
@@ -1,9 +1,19 @@
 <template lang="html">
-
+  <h1>Galgje</h1>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      woorden: [  'Kippenhok', 'Computer', 'Computer muis', 'Schommelbank', 'Grassprietje',
+                  'Alpaca', 'Vliegvakantie', 'Vakantie', 'Website', 'Hondenbakje',
+                  'Kreeft', 'Horoscoop', 'Televisie', 'Playstation', 'Gamer',
+                  'Winkelwagen', 'Hamburger', 'Tuinbroek', 'Appelsap', 'Birmingham',
+                  'Scooter', 'Stopcontactdoos', 'Autobandventieldopje', 'Deurklink', 'Brandweerauto',
+                  'Pakeerplaats', 'Huurcontract', 'Koopwoning', 'Programmeertalen', 'Klokwijzer'  ]
+    }
+  }
 }
 </script>
 
