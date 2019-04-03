29d3af9c73ab08d4404e23f2341d2c81a8095b4c
jasper <jasperguichelaar@outlook.com>
2019-04-03 10:45:47 +0200

    created a SubLetterComponent to set the letters

---
+++ ui/src/components/sub/SubLetterComponent.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <v-layout justify-center>
+      <v-card v-for="letter in letters" height="50px" width="50px">
+        <v-layout align-end justify-center row fill-height>
+          <v-card-text>
+            {{letter.letter}}
+          </v-card-text>
+        </v-layout>
+      </v-card>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      randomWord: 'dit woord',
+      letters: [],
+      cardWidth: null
+    }
+  },
+  methods: {
+    wordToArray () {
+      for (let i = 0; i < this.randomWord.length; i++) {
+        this.letters.push({'letter': this.randomWord.charAt(i), 'fisible': true})
+      }
+    }
+  },
+  created () {
+    this.wordToArray()
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
