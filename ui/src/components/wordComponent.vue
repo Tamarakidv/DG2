6f4dea252178aaae9caa144dc79e307e45e0f85d
TamaraKidv <97730@student.drenthecollege.nl>
2019-04-03 09:48:24 +0200

    Css file toegevoegd en de parent wordComponent DG2-6

---
+++ ui/src/components/wordComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ hangingmanWord }}</h1>
+</template>
+
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    hangingmanWord: String
+  }
+}
+</script>
--- ui/src/views/homePage.vue
+++ ui/src/views/homePage.vue
@@ -1,12 +1,26 @@
 <template lang="html">
-  <v-layout>
-    <h1>{{ gameName }}</h1>
-    <v-btn @click="randomWord">Start</v-btn>
+  <v-layout column>
+    <v-layout justify-center>
+      <h1 class="headerText">{{ gameName }}</h1>
+    </v-layout>
+    <v-layout justify-center>
+      <wordComponent
+        :hangingmanWord= "hangingmanWord"
+      ></wordcomponent>
+    </v-layout>
+    <v-layout justify-center>
+      <v-btn @click="randomWord">Start</v-btn>
+    </v-layout>
   </v-layout>
 </template>
 
 <script>
+import wordComponent from '../components/wordComponent.vue'
+
 export default {
+  components: {
+    wordComponent
+  },
   data () {
     return {
       gameName: 'Galgje',
