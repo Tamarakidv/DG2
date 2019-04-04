33d9d3aaf05455cb3c6ae0bd78d8c2d8b9d8b530
jasper <jasperguichelaar@outlook.com>
2019-04-04 14:08:46 +0200

    een text field en een button zijn gemaakt allen nog de validation -- DG2-8

---
+++ ui/src/components/sub/SubInputField.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <v-flex xs1>
+    <v-text-field
+      name="name"
+      label="kies een letter"
+      :rules="inputRule"
+    ></v-text-field>
+    <v-btn
+      color="primary"
+      :disabled="!valid"
+      @click="validate"
+    >
+      Enter
+    </v-btn>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      inputRule: [
+        v => (v && v.length <= 1) || 'je mag maar 1 letter per keer infullen'
+      ]
+    }
+  },
+  methods: {
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+      }
+    }
+  }
+  created () {
+    console.log();
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
