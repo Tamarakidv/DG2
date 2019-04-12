bb7b84a7d1fc0113fa6498cd7fe698c0db1be1fe
TamaraKidv <97730@student.drenthecollege.nl>
2019-04-12 09:11:23 +0200

    Speluitleg toegevoegd DG2-26

--- ui/src/components/base/BaseDialogComponentRules.vue
+++ ui/src/components/base/BaseDialogComponentRules.vue
@@ -2,18 +2,25 @@
   <v-dialog
     v-model="dialog"
     max-width="500px"
+    persistent
   >
     <v-card>
       <v-layout row wrap justify-center>
         <v-card-title primary-title class="headline">
-          Regels
+          <h3 class="headerText">Regels</h3>
         </v-card-title>
       </v-layout>
       <v-card-text>
-        1. regel 1
-        <br/>
-        2. regel 2
+        <p>Het spel genereert een woord. Je mag per beurt 1 letter invullen dat je denkt dat in het woord zit.
+        Zit de letter niet in het woord, krijg je een stukje galg erbij. </p>
+        <p>Je mag in totaal 5 keer fout raden.
+        Heb je 5 keer fout geraden, dan heb je verloren. Heb je het binnen 5 keer geraden, dan heb je gewonnen!</p>
       </v-card-text>
+      <v-card-actions>
+        <v-layout justify-center>
+          <v-btn color="primary" @click="closeDialog">Close</v-btn>
+        </v-layout>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -34,6 +41,11 @@ export default {
   computed: {
     checkGameRule () {
       return this.$store.state.gameRule
+    }
+  },
+  methods: {
+    closeDialog () {
+      this.dialog = false
     }
   }
 }
