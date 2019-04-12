0cca21e5df3c5b47e07b8eb19f19726d61504be4
jasper <jasperguichelaar@outlook.com>
2019-04-12 10:25:54 +0200

    gemaakt guesed letter -- DG2-24

--- ui/src/components/sub/SubGuessedLetterComponent.vue
+++ ui/src/components/sub/SubGuessedLetterComponent.vue
@@ -1,6 +1,15 @@
 <template lang="html">
-  <v-layout>
-    <img :src="require('@/assets/'+ guessedText)">
+  <v-layout column>
+    <v-layout row wrap>
+      <img :src="require('@/assets/'+ guessedText)">
+    </v-layout>
+      <v-layout row wrap>
+        <v-flex v-for="letter in letters" xs2>
+          <v-card-text >
+            {{letter}}
+          </v-card-text>
+        </v-flex>
+      </v-layout>
   </v-layout>
 </template>
 
@@ -9,12 +18,33 @@
 export default {
   data () {
     return {
+      letters: [],
+      counter: 0
     }
   },
   props: {
     guessedText: String
   },
-  methods: {
+  watch: {
+    checkLetters (data) {
+      let count = 0
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].letter != null) {
+          if (count == (data.length - 1)) {
+            this.letters.push(data[i].letter)
+          }else {
+            count++
+          }
+        } else {
+          count++
+        }
+      }
+    }
+  },
+  computed: {
+    checkLetters () {
+      return this.$store.state.storeGivenLetter
+    }
   }
 }
 </script>
